4e80a459b19a334359c9215b80443de4af92d7d5
BhimaMedia <120443958+BhimaTheYogi@users.noreply.github.com>
2022-12-24 17:12:12 -0800

    commit

--- UserProfil/urls.py
+++ UserProfil/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
     path('', views.index, name='UserProfil'),
 ]
--- UserProfil/views.py
+++ UserProfil/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
+def loginUser(request):
+    page = 'login'
+    if request.method == 'POST':
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None: 
+            login(request, user)
+            return redirect('Journal')
+        
+    return render(request, 'InvJournal/login_register.html', {'page':page})
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    context = {'form': form, 'page': page}
+    return render(request, 'InvJournal/login_register.html', context)
+
+@login_required(login_url='login')
 def index(request):
     return HttpResponse("Hello, world. You're at the Main Page")
